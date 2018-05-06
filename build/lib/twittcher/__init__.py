42dfc0e8289d3c9a46ee0ffb8f25919aedc41275
MDSK <oacgodkl21@gmail.com>
2018-05-06 15:41:40 +0800

    commit

--- build/lib/twittcher/__init__.py
+++
@@ -1,9 +0,0 @@
-""" twittcher/__init__.py """
-
-__all__ = ["PageWatcher", "UserWatcher", "SearchWatcher",
-	       "Tweet", "TweetSender"]
-
-from .twittcher import (PageWatcher, UserWatcher, SearchWatcher,
-	                    Tweet, TweetSender)
-
-from .version import __version__
--- tawa.py
+++
@@ -1,1 +0,0 @@
-print("tawawa123") 
